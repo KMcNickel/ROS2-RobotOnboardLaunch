29d1e01ccaecbb54baf43df85c532cb03e22973e
KMcNickel <37396126+KMcNickel@users.noreply.github.com>
2022-05-25 04:07:59 -0700

    Fix node start script

--- robot_onboard_launch/allNodesStart.sh
+++ robot_onboard_launch/allNodesStart.sh
@@ -18,7 +18,7 @@ ros2 lifecycle set /agv0/axis3/odrive activate
 ros2 lifecycle set /agv0/inverse_kinematics activate
 ros2 lifecycle set /agv0/forward_kinematics activate
 
-os2 service call /agv0/axis0/input/start std_srvs/srv/Trigger
-os2 service call /agv0/axis1/input/start std_srvs/srv/Trigger
-os2 service call /agv0/axis2/input/start std_srvs/srv/Trigger
-os2 service call /agv0/axis3/input/start std_srvs/srv/Trigger
+ros2 service call /agv0/axis0/input/start std_srvs/srv/Trigger
+ros2 service call /agv0/axis1/input/start std_srvs/srv/Trigger
+ros2 service call /agv0/axis2/input/start std_srvs/srv/Trigger
+ros2 service call /agv0/axis3/input/start std_srvs/srv/Trigger
