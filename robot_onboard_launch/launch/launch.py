9502d249bf85049c6b7f0c0a41ac91ae67603859
KMcNickel <37396126+KMcNickel@users.noreply.github.com>
2022-05-24 22:49:58 -0700

    Initial launch file

---
+++ robot_onboard_launch/launch/launch.py
@@ -0,0 +1,116 @@
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+deviceName = 'agv0'
+canbusInterfaceName = 'can0'
+frontLeftAxisNum = 0
+frontRightAxisNum = 1
+rearLeftAxisNum = 3
+rearRightAxisNum = 2
+
+wheelBaseWidth = 0.27178
+wheelBaseLength = 0.181356
+wheelRadius = 0.04064
+invertRight = True
+
+def generate_launch_description():
+    return LaunchDescription([
+    # SocketCAN
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('socketcan'),
+                    'launch',
+                    'socketcan_launch.py'
+                ])
+            ]),
+            launch_arguments = {
+                'device_name': deviceName,
+                'canbus_interface_name': canbusInterfaceName
+            }.items()
+        ),
+    # ODrive Front Left
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('odrive'),
+                    'launch',
+                    'odrive_launch.py'
+                ])
+            ]),
+            launch_arguments = {
+                'device_name': deviceName,
+                'canbus_interface_name': canbusInterfaceName,
+                'axis_number': frontLeftAxisNum
+            }.items()
+        ),
+    # ODrive Front Right
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('odrive'),
+                    'launch',
+                    'odrive_launch.py'
+                ])
+            ]),
+            launch_arguments = {
+                'device_name': deviceName,
+                'canbus_interface_name': canbusInterfaceName,
+                'axis_number': frontRightAxisNum
+            }.items()
+        ),
+    # ODrive Rear Left
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('odrive'),
+                    'launch',
+                    'odrive_launch.py'
+                ])
+            ]),
+            launch_arguments = {
+                'device_name': deviceName,
+                'canbus_interface_name': canbusInterfaceName,
+                'axis_number': rearLeftAxisNum
+            }.items()
+        ),
+    # ODrive Rear Right
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('odrive'),
+                    'launch',
+                    'odrive_launch.py'
+                ])
+            ]),
+            launch_arguments = {
+                'device_name': deviceName,
+                'canbus_interface_name': canbusInterfaceName,
+                'axis_number': rearRightAxisNum
+            }.items()
+        ),
+    # Kinematics
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('mecanum_kinematics'),
+                    'launch',
+                    'kinematic_launch.py'
+                ])
+            ]),
+            launch_arguments = {
+                'device_name': deviceName,
+                'front_left_axis_number': frontLeftAxisNum,
+                'front_right_axis_number': frontRightAxisNum,
+                'rear_left_axis_number': rearLeftAxisNum,
+                'rear_right_axis_number': rearRightAxisNum,
+                'wheel_base_width': wheelBaseWidth,
+                'wheel_base_length': wheelBaseLength,
+                'wheel_radius': wheelRadius,
+                'invert_right': invertRight
+            }.items()
+        )
+    ])
